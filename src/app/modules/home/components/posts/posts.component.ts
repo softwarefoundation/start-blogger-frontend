b155b71ee44d4a0de3dd34d4c5cb0f9a8153cc4f
Dherkyan R. Silva <dherkyan@gmail.com>
2024-08-19 03:54:05 -0300

    Listagem de posts

--- src/app/modules/home/components/posts/posts.component.ts
+++ src/app/modules/home/components/posts/posts.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PostService} from "../../../../shared/services/post.service";
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrl: './posts.component.scss'
 })
-export class PostsComponent {
+export class PostsComponent implements OnInit {
+
+  pagePosts: any = [];
+
+  constructor(private postService: PostService) {
+  }
+
+  ngOnInit(): void {
+    this.carregarListaInicialDePosts();
+  }
+
+  carregarListaInicialDePosts() {
+    this.postService.getPosts().subscribe(value => {
+      if (value.content) {
+        this.pagePosts = value
+      }
+    });
+  }
 
 }
--- src/app/shared/services/post.service.ts
+++ src/app/shared/services/post.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {PostModel} from "../models/post.model";
 import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,9 @@ export class PostService {
 
   constructor(private http: HttpClient) { }
 
+  getPosts():Observable<any>{
+    return this.http.get(`${this.baseUrl}`);
+  }
 
   postPost(post: PostModel){
     return this.http.post<PostModel>(`${this.baseUrl}`, post);
